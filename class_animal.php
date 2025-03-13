6ef31501aa94c85fb29eb94f0c2d1de1afc51cf7
Rafisetyanandika <rafinandika02@gmail.com>
2025-03-13 10:38:33 +0700

    praktikum04

--- class_animal.php
+++ class_animal.php
@@ -20,9 +20,13 @@ public function store($animal) {
     $this->animals[] = $animal;
 }
 
-public function update() {}
+public function update($index, $animal) {
+    $this->animals[$index] = $animal;
+}
 
-public function destory() {}
+public function destroy($index) {
+    unset($this->animals[$index]);
+}
 }
 
 $animal = new animal (["ayam", "ikan"]);
@@ -30,7 +34,20 @@ echo "index - menampilkan seluruh hewan <br/>";
 $animal->index();
 echo"<br/>";
 
+#method store
 echo "store - menampilkan seluruh hewan baru (burung) <br/>";
 $animal->store("burung");
+$animal->index();
+echo"<br/>";
+
+#method update
+echo "update - mengupdate hewan <br/>";
+$animal->update(0, "kucing anggora");
+$animal->index();
+echo"<br/>";
+
+#method destory
+echo "destroy - menghapus hewan <br/>";
+$animal->destroy(1);
 $animal->index();
 echo"<br/>";
