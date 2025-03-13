8b7e00bfd7dc7c3ff1c8b9e792735290d8a85c40
Rafisetyanandika <rafinandika02@gmail.com>
2025-03-13 21:40:22 +0700

    praktikum03

---
+++ praktikum03/task-praktikum03/task-praktikum03/startbootstrap-personal-gh-pages/partials/sections/experience-section.php
@@ -0,0 +1,27 @@
+<section>
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <h2 class="text-primary fw-bolder mb-0">Experience</h2>
+                                <!-- Download resume button-->
+                                <!-- Note: Set the link href target to a PDF file within your project-->
+                                <a class="btn btn-primary px-4 py-3" href="#!">
+                                    <div class="d-inline-block bi bi-download me-2"></div>
+                                    Download Resume
+                                </a>
+                            </div>
+                            <!-- Experience Card 1-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-primary fw-bolder mb-2">2021 - 2024</div>
+                                                <div class="small fw-bolder">BORCES</div>
+                                                <div class="small text-muted">DKV</div>
+                                                <div class="small text-muted">Bogor, Jawa Barat</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Pengalaman di jurusan Desain Komunikasi Visual (DKV) menawarkan kombinasi unik antara kreativitas, tantangan, dan pembelajaran praktis.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
